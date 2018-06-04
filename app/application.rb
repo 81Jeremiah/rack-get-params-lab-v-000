3b18cbb5f66e8e4775564f7e7033399d859864d4
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-06-04 02:22:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart = []
         resp.write "Your cart is empty"
-        binding.pry
+  
       else
         @@cart.each{|item|resp.write "#{item}\n"}
 
